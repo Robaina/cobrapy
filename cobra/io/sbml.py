854639f2ec41b6e8a857cd525d25216ab3ada132
ChristianLieven <clie@biosustain.dtu.dk>
2018-08-22 13:58:00 +0200

    fix: update documentation notebooks building_model and getting_started

--- cobra/io/sbml.py
+++ cobra/io/sbml.py
@@ -563,7 +563,7 @@ def get_libsbml_document(cobra_model,
         # they are set to be identical
         note_dict = the_reaction.notes.copy()
         if the_reaction.gene_reaction_rule:
-            note_dict['GENE ASSOCIATION'] = [
+            note_dict['GENE_ASSOCIATION'] = [
                 str(the_reaction.gene_reaction_rule)]
         if the_reaction.subsystem:
             note_dict['SUBSYSTEM'] = [str(the_reaction.subsystem)]
